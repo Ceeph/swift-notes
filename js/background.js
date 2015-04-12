dfeccfdb6f047f63a20e20150844e7f7cc8bd216
Alessandro Cifani <alessandro.cifani@gmail.com>
2015-04-12 23:28:42 +0200

    Defined the back-end structure

--- js/background.js
+++ js/background.js
@@ -1,7 +1,14 @@
-chrome.runtime.onInstalled.addListener(function(details){
-	alert("hi");
-	chrome.commands.onCommand.addListener(function(command) {
-	   alert(command);
+console.log("extension loaded");
+
+
+$(document).ready( function() {
+	chrome.browserAction.onClicked.addListener(function(tab) {
+		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { text: "get_panel" }, 
+         function(response){
+            alert(response.panel);
+            panel.togglePanel(response.panel);
+         });
+      });
 	});
 });
-
--- js/sidepanel.js
+++ js/sidepanel.js
@@ -1,3 +1,33 @@
-var iframe  = document.createElement ("iframe");
-iframe.src  = chrome.extension.getURL ("../sidebar.html");
-(document.head||document.documentElement).append(iframe);
+var panel =  {
+	/*loadHtml: function(){
+		$.get("../sidebar.html", function(data){
+			return data;
+		});
+	},*/
+
+	togglePanel: function(panel){
+		console.log("toggle");
+		var openPanel = function(){
+			//chrome.tabs.executeScript(null, {file: "js/content.js"});
+      alert("open panel");
+      //$('body').append("<div>CIAONE</div>");
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { text: "open_panel" }, 
+         function(response){
+            alert(response);
+         });
+      });
+		};
+
+		var closePanel = function(){
+      alert("close panel");
+      panel.remove();
+		};
+
+		if (panel) {			
+      closePanel();
+		} else {
+			openPanel();
+		}
+	}
+}
